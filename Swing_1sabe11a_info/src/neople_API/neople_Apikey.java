186068b2ec8a523a48eab9143d1c763a6c6bb8d9
1sabe11a <71987963+LeNoad@users.noreply.github.com>
2021-05-25 22:54:49 +0900

    Update neople_Apikey.java

--- Swing_1sabe11a_info/src/neople_API/neople_Apikey.java
+++ Swing_1sabe11a_info/src/neople_API/neople_Apikey.java
@@ -1,7 +1,7 @@
 package neople_API;
 
 public class neople_Apikey {
-    private String apiKey = "IBaD3W3StWlWlptNt7FvCLjUqZJjqcYw"; // 네오플 API 토큰값
+    private String apiKey = ""; // 네오플 API 토큰값
     public String setapiKey() {
         return apiKey;
     }
